05e2974c76b765c0406da50488bcbe296ab7aa05
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-04 12:48:09 +0600

    feat: add table models

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger("dev"));
+/* Middlewares. */
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({origin: '*'})); // allowing all the origin
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
