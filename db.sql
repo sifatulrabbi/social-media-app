7b7ab1aca16163dba3a2640f20b0304634623b38
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-05 16:18:52 +0600

    feat: complete the tables relations

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE profiles(
-    id INTEGER PRIMARY KEY, 
-    media INTEGER, 
-    type VARCHAR(255) NOT NULL, 
-    specialization VARCHAR(255) NOT NULL, 
-    colleagues INTEGER[] DEFAULT, 
-    attribute VARCHAR(255) NOT NULL, 
-    createdAt DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `userId` INTEGER, `organizationId` INTEGER, `orgId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT, FOREIGN KEY (`organizationId`) REFERENCES `organizations` (`id`) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (`orgId`) REFERENCES `organizations` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) ENGINE=InnoDB;
