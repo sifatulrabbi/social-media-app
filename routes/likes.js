fe31f46eacc0d38c8b146eaf2634a4749bf5dc9f
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-06 14:46:07 +0600

    refactor models and routers

--- routes/likes.js
+++ routes/likes.js
@@ -1,4 +1,4 @@
-const {Like, User} = require('../lib/models');
+const {Like} = require('../lib/models');
 const router = require('express').Router();
 
 /**
@@ -13,39 +13,6 @@ router.post('/', async (req, res, next) => {
         });
         // Send response
         res.status(200).json({success: true, data: like});
-    } catch (err) {
-        next(err);
-    }
-});
-
-/**
- * Get all the likes of a post
- */
-router.get('/?post=', async (req, res, next) => {
-    try {
-        const likes = await Like.findAll({
-            include: [User],
-        });
-        // Send response
-        res.status(200).json({success: true, data: likes});
-    } catch (err) {
-        next(err);
-    }
-});
-
-/**
- * Remove a like
- */
-router.delete('/:id', async (req, res, next) => {
-    try {
-        // Remove the like with the id
-        await Like.destroy({
-            where: {
-                id: req.params.id,
-            },
-        });
-        // Send response
-        res.status(200).json({success: true, message: 'Like removed'});
     } catch (err) {
         next(err);
     }
