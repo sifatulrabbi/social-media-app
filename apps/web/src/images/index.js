184a36f46cfba24055580183a63de21a39adc733
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-14 18:25:04 +0600

    feat: add home page section

---
+++ apps/web/src/images/index.js
@@ -0,0 +1,2 @@
+export {default as heroImg} from './hero.png';
+export {default as aboutImg} from './about.png';
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{tsx,ts,jsx,js}'],
+  theme: {
+    fontFamily: {
+      body: ['Montserrat', 'sans-serif'],
+      display: ['Roboto', 'sans-serif'],
+    },
+    colors: {
+      gray: colors.gray,
+      black: colors.black,
+      white: colors.white,
+      primary: '#44D752',
+      secondary: '#68C9BA',
+      textPrimary: '#212353',
+      textSecondary: '#4B5D68',
+    },
+  },
+  plugins: [],
+};
