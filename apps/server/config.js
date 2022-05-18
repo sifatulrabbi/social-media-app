7c0de0e8425ca9c18e5b58c76e62360c23b3f941
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-19 05:02:40 +0600

    add env files

--- apps/server/config.js
+++ apps/server/config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config({path: '.env'});
 
 module.exports = {
-  PORT: process.env.PORT || '8080',
+  PORT: process.env.NODE_DOCKER_PORT || '8080',
   DB_NAME: process.env.DB_NAME,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
