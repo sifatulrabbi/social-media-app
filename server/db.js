25f1b3e6a1c904463f057d92b380af5aa5f4926d
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-19 06:52:45 +0600

    feat: add docker

--- server/db.js
+++ server/db.js
@@ -7,7 +7,8 @@ const sequelize = new Sequelize(
   config.DB_USERNAME,
   config.DB_PASSWORD,
   {
-    host: 'localhost', // localhost as host server
+    host: config.DB_HOST, // localhost as host server
+    port: config.DB_PORT,
     dialect: 'mysql', // database dialect name
     logging: console.log, // using the console for logging
   },
